06c80f9969589b70206b0f06b8cec70cbe0e387a
ron555666 <165968242+ron555666@users.noreply.github.com>
2026-04-30 17:41:04 -0700

    edited database.py

--- app/database.py
+++ app/database.py
@@ -7,6 +7,11 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
+    
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set in .env")
 
